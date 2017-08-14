ef4d6e75e6e16fa455e88647a1f2dba8459042ef
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-14 15:50:33 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "dean")
+Genre.create(name: "pop")
+Song.create(name: "my way", artist_id: 1, genre_id: 1)
